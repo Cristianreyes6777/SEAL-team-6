48adf64f4116cdf5a9eadd75a2cec0110d3bf132
khinoj <kristopher.hinojosa@gmail.com>
2023-08-11 12:53:09 -0400

    updated js, html, images, and pulled stars wars api

--- assets/star_wars/js/starwars.js
+++ assets/star_wars/js/starwars.js
@@ -1,30 +1,46 @@
 searchInputEl = document.getElementById('searchInput');
-searchButtonEl = document.getElementById('searchButton');
+resultContainerEl = document.getElementById('resultContainer');
+selectTableEl = document.getElementById('selectTable');
+optionsEl = document.getElementsByClassName('options')
+nameBlockEl = document.getElementById('nameBlock');
 
-function starWarsDisplay() {
-    searchInputEl.addEventListener('keypress', function (event) {
-        if (event.key === 'Enter') {
-            pullStarWarsData();
-            event.preventDefault();
-        }
-    });
+let starWarsPicture = document.createElement('img');
+resultContainerEl.appendChild(starWarsPicture);
 
-    searchButtonEl.addEventListener('click', function (event) {
-        pullStarWarsData();
-        event.preventDefault();
-    });
-
-    function pullStarWarsData() {  
-            let requesturl = `https://swapi.dev/api/people/`;
-
-            fetch(requesturl)
-            .then(function (response) { 
-                return response.json();
-             })
-             .then(function (starData) {
-                console.log(starData);
-             })
+document.getElementById('selectTable').addEventListener('change', function (event) {
+    pullStarWarsData();
+    event.preventDefault();
+    let userChoose = selectTableEl.value;
+    console.log('test', userChoose);
+    if (userChoose != undefined) {
+        console.log('userchoose', userChoose)
+        starWarsPicture.src = './star_wars/images/' + userChoose + '.jpg'
     }
+});
+
+function pullStarWarsData() {
+    let requesturl = `https://swapi.dev/api/people/?search=${selectTableEl.value}`;
+
+    fetch(requesturl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (starData) {
+            console.log('data', starData);
+
+            const CharacterName = starData.results[0].name;
+            console.log('name', CharacterName);
+            const films = starData.results[0].films;
+            console.log('films', films);
+
+            let warsName = document.createElement('p');
+            warsName.textContent = CharacterName;
+            nameBlockEl.appendChild(warsName);
+        })
+    clearSearch()
 }
 
-starWarsDisplay();
+
+function clearSearch() {
+    nameBlockEl.textContent = '';
+}  
